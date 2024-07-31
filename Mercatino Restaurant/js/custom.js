801d3f5816537a7eb26a45f713cf659b9ab9a6c1
Taxal1810 <taxalpatel1810@gmail.com>
2024-07-31 11:23:55 +0530

    files upload

--- Mercatino Restaurant/js/custom.js
+++ Mercatino Restaurant/js/custom.js
@@ -1,4 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
+/****************************Header***********************************/
+
+
+jQuery(document).ready(function () {
+    jQuery('.mobileIcon').click(function () {
+        jQuery("body").toggleClass('menu-open');
+    });
+});
+
+
+/****************** gallary slider ********************/ 
+
+var swiper = new Swiper(".gallary_slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    slidesPerView: 4.5,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".next_arrow",
+      prevEl: ".previous_arrow",
+    },
+  });
+
+
+  /******************** about slider ********************/
+  
+ 
     var swiper = new Swiper(".about_slider", {
         slidesPerView: 1,
         spaceBetween: 30, // Add space between slides if needed
@@ -10,4 +44,4 @@ document.addEventListener('DOMContentLoaded', function () {
             prevEl: ".prev_btn",
         },
     });
-});
+
